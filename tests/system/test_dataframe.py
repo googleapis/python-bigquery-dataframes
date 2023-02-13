37e5bbe3c31c9638a9f6bb264693289fb40624e2
Garrett Wu <garrettwu@google.com>
2023-02-13 22:41:37 +0000

    modify dataframe tests againest scalars_pandas_df, add test on a single string selection
    Change-Id: I7b15a4308cf5af8cab1f9bc6888b57e0b8d585ee

--- tests/system/test_dataframe.py
+++ tests/system/test_dataframe.py
@@ -1,9 +1,17 @@
-import pandas.testing
+import pandas as pd
 
 
-def test_get_columns(scalars_df):
-    df_subset = scalars_df[["bool_col", "float64_col", "int64_col"]]
+def test_get_column(scalars_df, scalars_pandas_df):
+    col_name = "int64_col"
+    series = scalars_df[col_name]
+    series_pandas = series.compute()
+    pd.testing.assert_series_equal(series_pandas, scalars_pandas_df[col_name])
+
+
+def test_get_columns(scalars_df, scalars_pandas_df):
+    col_names = ["bool_col", "float64_col", "int64_col"]
+    df_subset = scalars_df[col_names]
     df_pandas = df_subset.compute()
-    pandas.testing.assert_index_equal(
-        df_pandas.columns, pandas.Index(["bool_col", "float64_col", "int64_col"])
+    pd.testing.assert_index_equal(
+        df_pandas.columns, scalars_pandas_df[col_names].columns
     )
