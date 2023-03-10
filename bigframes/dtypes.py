074c5cbec6be398da4077a679667d2e4d43ab4aa
Brenton Milne <bmil@google.com>
2023-03-10 19:23:22 +0000

    add custom type hints for dtypes
    Change-Id: I7a9e5087b72a8aec6cced16efde2d071bfca68dc

--- bigframes/dtypes.py
+++ bigframes/dtypes.py
@@ -1,24 +1,58 @@
 """Mappings for Pandas dtypes supported by BigFrames"""
 
+from typing import Any, Dict, Iterable, Tuple, Union
+
 import db_dtypes  # type: ignore
 import ibis.expr.datatypes as ibis_dtypes
 import numpy as np
 import pandas as pd
 
-BIDIRECTIONAL_MAPPINGS = (
+# Type hints for Pandas dtypes supported by BigFrames
+BigFramesDtype = Union[
+    pd.BooleanDtype,
+    pd.Float64Dtype,
+    pd.Int64Dtype,
+    pd.StringDtype,
+    db_dtypes.DateDtype,
+    db_dtypes.TimeDtype,
+    np.dtype[np.datetime64],
+    pd.DatetimeTZDtype,
+]
+
+# Type hints for Ibis data types supported by BigFrames
+IbisDtype = Union[
+    ibis_dtypes.Boolean,
+    ibis_dtypes.Float64,
+    ibis_dtypes.Int64,
+    ibis_dtypes.String,
+    ibis_dtypes.Date,
+    ibis_dtypes.Time,
+    ibis_dtypes.Timestamp,
+]
+
+# Type hints for Ibis data types that can be read to Python objects by BigFrames
+ReadOnlyIbisDtype = Union[
+    ibis_dtypes.Binary, ibis_dtypes.JSON, ibis_dtypes.Decimal, ibis_dtypes.GeoSpatial
+]
+
+BIDIRECTIONAL_MAPPINGS: Iterable[Tuple[IbisDtype, BigFramesDtype]] = (
     (ibis_dtypes.boolean, pd.BooleanDtype()),
     (ibis_dtypes.float64, pd.Float64Dtype()),
     (ibis_dtypes.int64, pd.Int64Dtype()),
-    (ibis_dtypes.string, pd.StringDtype()),
+    (ibis_dtypes.string, pd.StringDtype(storage=None)),
     (ibis_dtypes.date, db_dtypes.DateDtype()),
     (ibis_dtypes.time, db_dtypes.TimeDtype()),
     (ibis_dtypes.timestamp, np.dtype("datetime64[us]")),
     (ibis_dtypes.Timestamp(timezone="UTC"), pd.DatetimeTZDtype(unit="us", tz="UTC")),  # type: ignore
 )
 
-BIGFRAMES_TO_IBIS = {pandas: ibis for ibis, pandas in BIDIRECTIONAL_MAPPINGS}
+BIGFRAMES_TO_IBIS: Dict[BigFramesDtype, IbisDtype] = {
+    pandas: ibis for ibis, pandas in BIDIRECTIONAL_MAPPINGS
+}
 
-IBIS_TO_BIGFRAMES = {ibis: pandas for ibis, pandas in BIDIRECTIONAL_MAPPINGS}
+IBIS_TO_BIGFRAMES: Dict[
+    Union[IbisDtype, ReadOnlyIbisDtype], Union[BigFramesDtype, np.dtype[Any]]
+] = {ibis: pandas for ibis, pandas in BIDIRECTIONAL_MAPPINGS}
 IBIS_TO_BIGFRAMES.update(
     {
         ibis_dtypes.binary: np.dtype("O"),
@@ -33,7 +67,9 @@ IBIS_TO_BIGFRAMES.update(
 )
 
 
-def ibis_dtype_to_bigframes_dtype(ibis_dtype: ibis_dtypes.DataType):
+def ibis_dtype_to_bigframes_dtype(
+    ibis_dtype: Union[IbisDtype, ReadOnlyIbisDtype]
+) -> Union[BigFramesDtype, np.dtype[Any]]:
     """Converts an Ibis dtype to a BigFrames dtype
 
     Args:
@@ -53,7 +89,7 @@ def ibis_dtype_to_bigframes_dtype(ibis_dtype: ibis_dtypes.DataType):
         raise ValueError("Unexpected Ibis data type")
 
 
-def bigframes_dtype_to_ibis_dtype(bigframes_dtype):
+def bigframes_dtype_to_ibis_dtype(bigframes_dtype: BigFramesDtype) -> IbisDtype:
     """Converts a BigFrames supported dtype to an Ibis dtype
 
     Args:
