4ba47f5a2878b29c02fba82019adb88db56daa57
Bradford Orr <orrbradford@google.com>
2023-08-07 16:43:21 +0000

    feat: add `bigframes.options.compute.maximum_bytes_billed` option that sets maximum bytes billed on query jobs
    -implement context manager for global options
    -maximum_bytes_billed only applies to query jobs. This limitation will be set per query. Operations that trigger multiple jobs may result in total usage beyond this setting

--- tests/unit/test_compute_options.py
+++ tests/unit/test_compute_options.py
@@ -1,13 +1,29 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import bigframes as bf
 
+from . import resources
 
-def test_maximum_bytes_option(session: bf.Session, mock_bigquery_client):
+
+def test_maximum_bytes_option():
+    session = resources.create_bigquery_session()
     num_query_calls = 0
     with bf.option_context("compute.maximum_bytes_billed", 10000):
         # clear initial method calls
-        mock_bigquery_client.method_calls = []
+        session.bqclient.method_calls = []
         session._start_query("query")
-        for call in mock_bigquery_client.method_calls:
+        for call in session.bqclient.method_calls:
             name, _, kwargs = call
             if name == "query":
                 num_query_calls = +1
