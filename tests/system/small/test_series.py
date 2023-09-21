4284d8f3686a6ffada40f00e96f5e4f66c5e30f3
Henry J Solberg <henryjsolberg@google.com>
2023-09-21 16:51:31 +0000

    add scalar case and update return types

--- tests/system/small/test_series.py
+++ tests/system/small/test_series.py
@@ -2617,7 +2617,7 @@ def test_loc_single_index_no_duplicate(scalars_df_index, scalars_pandas_df_index
     index = -2345
     bf_result = scalars_df_index.date_col.loc[index]
     pd_result = scalars_pandas_df_index.date_col.loc[index]
-    assert bf_result.to_pandas().iloc[0] == pd_result
+    assert bf_result == pd_result
 
 
 def test_series_bool_interpretation_error(scalars_df_index):
