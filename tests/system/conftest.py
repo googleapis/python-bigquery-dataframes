f2e593d7b2fcd3cbc73ca8293e7586fff7aaa0fc
Trevor Bergeron <tbergeron@google.com>
2023-10-16 22:43:13 +0000

    feat: add unstack to series, add `level` param

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -388,7 +388,11 @@ def hockey_df(
     hockey_table_id: str, session: bigframes.Session
 ) -> bigframes.dataframe.DataFrame:
     """DataFrame pointing at test data."""
-    return session.read_gbq(hockey_table_id)
+    return (
+        session.read_gbq(hockey_table_id)
+        .set_index(["player_name", "season"])
+        .sort_index()
+    )
 
 
 @pytest.fixture(scope="session")
@@ -407,7 +411,7 @@ def hockey_pandas_df() -> pd.DataFrame:
             "season": pd.Int64Dtype(),
         },
     )
-    df.index = df.index.astype("Int64")
+    df = df.set_index(["player_name", "season"]).sort_index()
     return df
 
 
