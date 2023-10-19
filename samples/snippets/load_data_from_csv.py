f32eac8df4969c8fe03e0562424a6b16cd1346d8
Ashley Xu <ashleyxu@google.com>
2023-10-19 03:16:11 +0000

    fix: create standalone, testable, and runnable code snippets

--- samples/snippets/load_data_from_csv.py
+++ samples/snippets/load_data_from_csv.py
@@ -12,20 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 
-import bigframes.pandas
+def test_bigquery_dataframes_load_data_from_csv():
+    # [START bigquery_dataframes_load_data_from_csv]
+    import bigframes.pandas as bpd
 
-from . import sample
-
-
-def test_remote_function_and_read_gbq_function(
-    capsys: pytest.CaptureFixture[str],
-) -> None:
-    # We need a fresh session since we're modifying connection options.
-    bigframes.pandas.close_session()
-
-    # TODO(swast): Get project from environment so contributors can run tests.
-    sample.use_bigquery_dataframes("bigframes-dev")
-    out, _ = capsys.readouterr()
-    assert "average_body_mass: " in out
+    filepath_or_buffer = (
+        "gs://bigquery-public-data-ml-datasets/holidays_and_events_for_forecasting.csv"
+    )
+    df_from_gcs = bpd.read_csv(filepath_or_buffer)
+    # Display the first few rows of the DataFrame:
+    df_from_gcs.head()
+    # [END bigquery_dataframes_load_data_from_csv]
+    assert df_from_gcs is not None
--- samples/snippets/pandas_methods.py
+++ samples/snippets/pandas_methods.py
@@ -13,35 +13,14 @@
 # limitations under the License.
 
 
-def use_bigquery_dataframes(project_id: str):
-    # [START bigquery_dataframes_set_options]
+def test_bigquery_dataframes_pandas_methods():
+    # [START test_bigquery_dataframes_pandas_methods]
     import bigframes.pandas as bpd
 
-    PROJECT_ID = project_id  # @param {type:"string"}
-    REGION = "US"  # @param {type:"string"}
-
-    # Set BigQuery DataFrames options
-    bpd.options.bigquery.project = PROJECT_ID
-    bpd.options.bigquery.location = REGION
-
-    # [END bigquery_dataframes_set_options]
-
-    # [START bigquery_dataframes_load_data_from_bigquery]
-    # Create a DataFrame from a BigQuery table:
+    # Load data from BigQuery
     query_or_table = "bigquery-public-data.ml_datasets.penguins"
     bq_df = bpd.read_gbq(query_or_table)
-    # [END bigquery_dataframes_load_data_from_bigquery]
 
-    # [START bigquery_dataframes_load_data_from_csv]
-    filepath_or_buffer = (
-        "gs://bigquery-public-data-ml-datasets/holidays_and_events_for_forecasting.csv"
-    )
-    df_from_gcs = bpd.read_csv(filepath_or_buffer)
-    # Display the first few rows of the DataFrame:
-    df_from_gcs.head()
-    # [END bigquery_dataframes_load_data_from_csv]
-
-    # [START bigquery_dataframes_inspect_and_manipulate_data]
     # Inspect one of the columns (or series) of the DataFrame:
     bq_df["body_mass_g"].head(10)
 
@@ -51,6 +30,4 @@ def use_bigquery_dataframes(project_id: str):
 
     # Calculate the mean body_mass_g by species using the groupby operation:
     bq_df["body_mass_g"].groupby(by=bq_df["species"]).mean().head()
-    # [END bigquery_dataframes_inspect_and_manipulate_data]
-
-    # TODO(ashleyxu): Add samples for loading DataFrames to BigQuery table.
+    # [END test_bigquery_dataframes_pandas_methods]
