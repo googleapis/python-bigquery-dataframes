4e7d81c5e8ff05eeb1a99a550c87cbf47fb9fa9b
Your Name <stabd@google.com>
2023-10-25 11:24:23 -0500

    Creating clarifying comments

--- samples/snippets/bqml_getting_started_test.py
+++ samples/snippets/bqml_getting_started_test.py
@@ -13,12 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-def test_bqml_getting_started():
 
+def test_bqml_getting_started():
+    #[START bigquery_getting_Started_bqml_tutorial]
+    #DataFrame created from a BigQuery table:
     import bigframes.pandas as bpd 
     import bigframes 
 
-#
+    # Original sql query from tutorial, translated to Python using BigQuery BigFrames dataframes 
     df = bpd.read_gbq('''
     SELECT GENERATE_UUID() AS rowindex, *
     FROM
@@ -28,39 +30,42 @@ def test_bqml_getting_started():
     ''',
     index_col='rowindex')
 
-    # make comments 
-
+    #Printing dataframe, setting totals value 
     totals = df['totals']
 
-    #using totals, selecting id for transaction example 
+    #Using totals, selecting id for transaction example 
     totals['0000fb2c-2861-40be-9c6c-309afd7e7883']
-
     transactions = totals.struct.field("transactions")
-
+    #Columns to indicate whether there was purchase 
     label = transactions.notnull().map({True: 1, False: 0})
 
+    #Operating systems of users
     operatingSystem = df['device'].struct.field("operatingSystem")
-
     operatingSystem = operatingSystem.fillna("")
 
+    #Indicates whether the users devices are mobile 
     isMobile = df['device'].struct.field("isMobile")
 
+    #Country from which the sessions originate, IP address based 
     country = df['geoNetwork'].struct.field("country").fillna("")
 
+    #Total number of pageviews within the session
     pageviews = totals.struct.field("pageviews").fillna(0)
 
+    #Setting features for dataframe, 
     features = bpd.DataFrame({
         'os': operatingSystem,
         'is_mobile': isMobile,
         'pageviews': pageviews
     })
 
-    # printing out the dataframe 
+    #Printing out the dataframe 
     df 
-
+    
+    #Creating a logistics regression model - 
     from bigframes.ml.linear_model import LogisticRegression
-
     model = LogisticRegression()
-
+    #Model training parameters, 
     model.fit(features, label)
+    #Write a DataFRame to a BigQuery table- 
     model.to_gbq('bqml_tutorial.sample_model', replace = True)
