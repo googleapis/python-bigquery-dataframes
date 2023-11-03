0f4de8190de82cc723fca1f16ca28c78a06aef5f
Ashley Xu <ashleyxu@google.com>
2023-11-03 22:51:27 +0000

    fix: use random table when loading data for read_csv, read_json, read_parquet

--- bigframes/session/__init__.py
+++ bigframes/session/__init__.py
@@ -1026,7 +1026,7 @@ class Session(
         encoding: Optional[str] = None,
         **kwargs,
     ) -> dataframe.DataFrame:
-        table = bigquery.Table(self._create_session_table())
+        table = bigframes_io.random_table(self._anonymous_dataset)
 
         if engine is not None and engine == "bigquery":
             if any(param is not None for param in (dtype, names)):
@@ -1140,7 +1140,7 @@ class Session(
         # Note: "engine" is omitted because it is redundant. Loading a table
         # from a pandas DataFrame will just create another parquet file + load
         # job anyway.
-        table = bigquery.Table(self._create_session_table())
+        table = bigframes_io.random_table(self._anonymous_dataset)
 
         job_config = bigquery.LoadJobConfig()
         job_config.create_disposition = bigquery.CreateDisposition.CREATE_IF_NEEDED
@@ -1163,7 +1163,7 @@ class Session(
         engine: Literal["ujson", "pyarrow", "bigquery"] = "ujson",
         **kwargs,
     ) -> dataframe.DataFrame:
-        table = bigquery.Table(self._create_session_table())
+        table = bigframes_io.random_table(self._anonymous_dataset)
 
         if engine == "bigquery":
 
