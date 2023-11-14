1c3deb5c6b473ca7e65ddc30e6bea62ca6e737fe
Ashley Xu <ashleyxu@google.com>
2023-11-14 18:09:12 +0000

    Make the global _api_methods and lock threads

--- tests/unit/core/test_log_adapter.py
+++ tests/unit/core/test_log_adapter.py
@@ -8,7 +8,7 @@ MAX_LABELS_COUNT = 64
 @pytest.fixture
 def test_instance():
     # Create a simple class for testing
-    @log_adapter.class_logger()
+    @log_adapter.class_logger
     class TestClass:
         def method1(self):
             pass
@@ -19,29 +19,26 @@ def test_instance():
     return TestClass()
 
 
-def test_class_logger_decorator(test_instance):
-    # Ensure that the class logger decorator adds _api_methods attribute
-    assert hasattr(test_instance, "_api_methods")
-    assert test_instance._api_methods == []
+def test_method_logging(test_instance):
+    test_instance.method1()
+    test_instance.method2()
 
-
-def test_add_api_method(test_instance):
-    # Ensure that add_api_method correctly adds a method to _api_methods
-    log_adapter.add_api_method("method3", test_instance)
-    assert test_instance._api_methods == ["method3"]
+    # Check if the methods were added to the _api_methods list
+    api_methods = log_adapter.get_and_reset_api_methods()
+    assert api_methods == ["method2", "method1"]
 
 
 def test_add_api_method_limit(test_instance):
     # Ensure that add_api_method correctly adds a method to _api_methods
     for i in range(70):
-        log_adapter.add_api_method("method3", test_instance)
-    assert len(test_instance._api_methods) == MAX_LABELS_COUNT
+        test_instance.method2()
+    assert len(log_adapter._api_methods) == MAX_LABELS_COUNT
 
 
 def test_get_and_reset_api_methods(test_instance):
     # Ensure that get_and_reset_api_methods returns a copy and resets the list
     test_instance.method1()
     test_instance.method2()
-    previous_methods = log_adapter.get_and_reset_api_methods(test_instance)
-    assert previous_methods == ["method2", "method1"]
-    assert test_instance._api_methods == []
+    previous_methods = log_adapter.get_and_reset_api_methods()
+    assert previous_methods is not None
+    assert log_adapter._api_methods == []
