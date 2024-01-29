cd578725fa760bcc24bc354d9401092ec5ddecaa
Tim Swast <swast@google.com>
2024-01-29 15:10:21 -0600

    set max instead of exact

--- noxfile.py
+++ noxfile.py
@@ -30,7 +30,7 @@ import nox.sessions
 
 BLACK_VERSION = "black==22.3.0"
 ISORT_VERSION = "isort==5.12.0"
-PYTEST_VERSION = "pytest==7.4.4"
+PYTEST_VERSION = "pytest<=7.4.4"
 SPHINX_VERSION = "sphinx==4.5.0"
 LINT_PATHS = ["docs", "bigframes", "tests", "third_party", "noxfile.py", "setup.py"]
 
