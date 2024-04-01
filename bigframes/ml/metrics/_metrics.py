13a5d5328e7110b9204fd56e2ee1945983a8288e
Garrett Wu <garrettwu@google.com>
2024-04-01 22:21:21 +0000

    fix docs

--- bigframes/ml/metrics/_metrics.py
+++ bigframes/ml/metrics/_metrics.py
@@ -344,3 +344,8 @@ def mean_squared_error(
     y_true_series, y_pred_series = utils.convert_to_series(y_true, y_pred)
 
     return (y_pred_series - y_true_series).pow(2).sum() / len(y_true_series)
+
+
+mean_squared_error.__doc__ = inspect.getdoc(
+    vendored_metrics_regression.mean_squared_error
+)
