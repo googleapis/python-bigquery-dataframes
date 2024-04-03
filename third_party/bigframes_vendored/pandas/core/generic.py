0e836161ec4e7734d929deb8b6d57e461e7897ff
Tim Swast <swast@google.com>
2024-04-03 20:08:36 +0000

    fix lint

--- third_party/bigframes_vendored/pandas/core/generic.py
+++ third_party/bigframes_vendored/pandas/core/generic.py
@@ -167,7 +167,7 @@ class NDFrame(indexing.IndexingMixin):
             dtype (str or pandas.ExtensionDtype):
                 A dtype supported by BigQuery DataFrame include ``'boolean'``, ``'Float64'``, ``'Int64'``,
                 ``'int64[pyarrow]'``, ``'string'``, ``'string[pyarrow]'``, ``'timestamp[us, tz=UTC][pyarrow]'``,
-                ``'timestamp\[us\]\[pyarrow\]'``, ``'date32\[day\]\[pyarrow\]'``, ``'time64\[us\]\[pyarrow\]'``.
+                ``'timestamp[us][pyarrow]'``, ``'date32[day][pyarrow]'``, ``'time64[us][pyarrow]'``.
                 A pandas.ExtensionDtype include ``pandas.BooleanDtype()``, ``pandas.Float64Dtype()``,
                 ``pandas.Int64Dtype()``, ``pandas.StringDtype(storage="pyarrow")``,
                 ``pd.ArrowDtype(pa.date32())``, ``pd.ArrowDtype(pa.time64("us"))``,
