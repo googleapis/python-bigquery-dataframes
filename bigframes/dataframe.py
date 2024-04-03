6d0217de3408e52382eb5c998c9cb387a127e570
Henry Solberg <henry.j.solberg@gmail.com>
2024-04-03 08:54:53 -0700

    Update bigframes/dataframe.py
    Co-authored-by: Tim Sweña (Swast) <swast@google.com>

--- bigframes/dataframe.py
+++ bigframes/dataframe.py
@@ -2667,9 +2667,10 @@ class DataFrame(vendored_pandas_frame.DataFrame):
             if_exists = "replace"
 
             temp_table_ref = self._session._random_table(
-                skip_cleanup=True
-            )  # the client code owns
-            # this table reference now, so skip_cleanup=True to not clean it up when we close the session
+                # The client code owns this table reference now, so skip_cleanup=True
+                #  to not clean it up when we close the session.
+                skip_cleanup=True,
+            )
             destination_table = f"{temp_table_ref.project}.{temp_table_ref.dataset_id}.{temp_table_ref.table_id}"
 
         table_parts = destination_table.split(".")
