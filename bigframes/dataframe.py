6057c4083e8bcf5e7d218d46c3ac692dc3078a39
Huan Chen <huanc@google.com>
2024-04-09 18:14:32 +0000

    Update code logic

--- bigframes/dataframe.py
+++ bigframes/dataframe.py
@@ -2143,20 +2143,20 @@ class DataFrame(vendored_pandas_frame.DataFrame):
         columns: typing.Union[blocks.Label, Sequence[blocks.Label]] = None,
         aggfunc: str = "mean",
     ) -> DataFrame:
-        if isinstance(index, blocks.Label) and index in self.columns:
-            index = [index]
-        else:
+        if isinstance(index, Iterable) and not (isinstance(index, blocks.Label) and index in self.columns):
             index = list(index)
-
-        if isinstance(columns, blocks.Label) and columns in self.columns:
-            columns = [columns]
         else:
+            index = [index]
+
+        if isinstance(columns, Iterable) and not (isinstance(columns, blocks.Label) and columns in self.columns):
             columns = list(columns)
-
-        if isinstance(values, blocks.Label) and values in self.columns:
-            values = [values]
         else:
+            columns = [columns]
+
+        if isinstance(values, Iterable) and not (isinstance(values, blocks.Label) and values in self.columns):
             values = list(values)
+        else:
+            values = [values]
 
         # Unlike pivot, pivot_table has values always ordered.
         values.sort()
