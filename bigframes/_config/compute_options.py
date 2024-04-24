026226e786e24fc496c69962fd0c1de8e3622f10
Huan Chen <huanc@google.com>
2024-04-24 23:11:14 +0000

    Update docstring

--- bigframes/_config/compute_options.py
+++ bigframes/_config/compute_options.py
@@ -44,7 +44,7 @@ class ComputeOptions:
             If enabled, large queries may be factored into multiple smaller queries
             in order to avoid generating queries that are too complex for the query
             engine to handle. However this comes at the cost of increase cost and latency.
-        extra_query_labels (Dict[str, Any]):
+        extra_query_labels (Dict[str, Any], Options):
             Stores additional custom labels for query configuration.
     """
 
@@ -52,8 +52,22 @@ class ComputeOptions:
     enable_multi_query_execution: bool = False
     extra_query_labels: Dict[str, Any] = dataclasses.field(default_factory=dict)
 
-    def assign_extra_query_labels(self, **kwargs):
-        reserved_keys = {"maximum_bytes_billed", "enable_multi_query_execution"}
+    def assign_extra_query_labels(self, **kwargs: Any) -> None:
+        """
+        Assigns additional custom labels for query configuration. The method updates the
+        `extra_query_labels` dictionary with new labels provided through keyword arguments.
+
+        Args:
+            kwargs (Any):
+                Custom labels provided as keyword arguments. Each key-value pair
+                in `kwargs` represents a label name and its value.
+
+        Raises:
+            ValueError: If a key matches one of the reserved attribute names,
+                specifically 'maximum_bytes_billed' or 'enable_multi_query_execution',
+                to prevent conflicts with built-in settings.
+        """
+        reserved_keys = ["maximum_bytes_billed", "enable_multi_query_execution"]
         for key in kwargs:
             if key in reserved_keys:
                 raise ValueError(
