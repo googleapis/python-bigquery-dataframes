c06f49222138c6a6c550ecbb9c634f7b3116b7a0
milkshakeiii <milkshakeiii@gmail.com>
2024-05-06 18:17:39 +0000

    fix comment

--- tests/system/small/test_multiindex.py
+++ tests/system/small/test_multiindex.py
@@ -1207,6 +1207,6 @@ def test_column_multi_index_w_na_stack(scalars_df_index, scalars_pandas_df_index
     pd_result = pd_df.stack()
     bf_result = bf_df.stack().to_pandas()
 
-    # Pandas produces NaN, where bq dataframes produces pd.NA
+    # Pandas produces pd.NA, where bq dataframes produces NaN
     pd_result["c"] = pd_result["c"].replace(pandas.NA, np.nan)
     pandas.testing.assert_frame_equal(bf_result, pd_result, check_dtype=False)
