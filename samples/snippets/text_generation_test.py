e846f2d2e4fb7248fc9e47dd251e4d5cb441acf6
Ashley Xu <ashleyxu@google.com>
2024-05-09 17:25:28 +0000

    address more comments

--- samples/snippets/text_generation_test.py
+++ samples/snippets/text_generation_test.py
@@ -19,9 +19,9 @@ def test_llm_text_generation() -> None:
     import os
 
     PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT", "bigframes-dev")
-    LOCATION = "us"
+    LOCATION = "US"
 
-    # [START bigquery_dataframes_create_remote_model]
+    # [START bigquery_dataframes_generate_text_tutorial_create_remote_model]
     import bigframes
     from bigframes.ml.llm import PaLM2TextGenerator
 
@@ -29,33 +29,40 @@ def test_llm_text_generation() -> None:
     bigframes.options.bigquery.location = LOCATION
 
     model = PaLM2TextGenerator()
-    # [END bigquery_dataframes_create_remote_model]
+    # [END bigquery_dataframes_generate_text_tutorial_create_remote_model]
     assert model is not None
 
-    # [START bigquery_dataframes_perform_keyword_extraction]
+    # [START bigquery_dataframes_generate_text_tutorial_perform_keyword_extraction]
     import bigframes.pandas as bpd
 
-    df = bpd.read_gbq("bigquery-public-data.imdb.reviews").head(n=5)
+    df = bpd.read_gbq("bigquery-public-data.imdb.reviews", max_results=5)
     df_prompt_prefix = "Extract the key words from the text below: "
     df_prompt = df_prompt_prefix + df["review"]
 
     # Predict using the model
     df_pred = model.predict(df_prompt, temperature=0.2, max_output_tokens=100)
     df_pred.peek(5)
-    # [END bigquery_dataframes_perform_keyword_extraction]
+    # [END bigquery_dataframes_generate_text_tutorial_perform_keyword_extraction]
+    # peek() is used to show a preview of the results. If the output
+    # of this sample changes, also update the screenshot for the associated
+    # tutorial on cloud.google.com.
     assert df_pred["ml_generate_text_llm_result"] is not None
     assert df_pred["ml_generate_text_llm_result"].iloc[0] is not None
 
-    # [START bigquery_dataframes_perform_sentiment_analysis]
+    # [START bigquery_dataframes_generate_text_tutorial_perform_sentiment_analysis]
     import bigframes.pandas as bpd
 
-    df = bpd.read_gbq("bigquery-public-data.imdb.reviews").head(n=5)
+    df = bpd.read_gbq("bigquery-public-data.imdb.reviews", max_results=5)
     df_prompt_prefix = "perform sentiment analysis on the following text, return one the following categories: positive, negative: "
     df_prompt = df_prompt_prefix + df["review"]
 
     # Predict using the model
     df_pred = model.predict(df_prompt, temperature=0.2, max_output_tokens=100)
     df_pred.peek(5)
-    # [END bigquery_dataframes_perform_sentiment_analysis]
+    # [END bigquery_dataframes_generate_text_tutorial_perform_sentiment_analysis]
+    # peek() is used to show a preview of the results. If the output
+    # of this sample changes, also update the screenshot for the associated
+    # tutorial on cloud.google.com.
+
     assert df_pred["ml_generate_text_llm_result"] is not None
     assert df_pred["ml_generate_text_llm_result"].iloc[0] is not None
