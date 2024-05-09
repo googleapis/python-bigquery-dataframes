ea9b120fc15ec83a79aa115f50394f9935d04899
Trevor Bergeron <tbergeron@google.com>
2024-05-09 18:07:21 +0000

    fix tests

--- tests/system/small/test_empty_index.py
+++ tests/system/small/test_empty_index.py
@@ -21,7 +21,7 @@ def test_empty_index_series_repr(
     pd_result = (
         scalars_pandas_df_default_index["int64_too"]
         .head(5)
-        .to_string(dtype=True, index=False, name=True)
+        .to_string(dtype=True, index=False, length=True, name=True)
     )
     assert bf_result == pd_result
 
@@ -85,7 +85,9 @@ def test_empty_index_groupby_aggregate(
 def test_empty_index_analytic(scalars_df_empty_index, scalars_pandas_df_default_index):
     bf_result = scalars_df_empty_index["int64_col"].cumsum().to_pandas()
     pd_result = scalars_pandas_df_default_index["int64_col"].cumsum()
-    pd.testing.assert_series_equal(bf_result, pd_result.reset_index(drop=True))
+    pd.testing.assert_series_equal(
+        bf_result, pd_result.reset_index(drop=True), check_dtype=False
+    )
 
 
 def test_empty_index_groupby_analytic(
