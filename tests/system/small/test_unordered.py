7af0c841a6913bf08c04f97c5ad027ce0e0564f4
Trevor Bergeron <tbergeron@google.com>
2024-06-17 17:24:45 +0000

    ignore_order in unordered test

--- tests/system/small/test_unordered.py
+++ tests/system/small/test_unordered.py
@@ -57,4 +57,4 @@ def test_unordered_mode_read_gbq(unordered_session):
             ),
         }
     )
-    assert_pandas_df_equal(df.to_pandas(), expected)
+    assert_pandas_df_equal(df.to_pandas(), expected, ignore_order=True)
