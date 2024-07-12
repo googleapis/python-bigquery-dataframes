d877261dac9f4910e39f9d2c75b564a6cb3d1646
Tim Swast <swast@google.com>
2024-07-12 21:00:38 +0000

    fix inmemorytable

--- third_party/bigframes_vendored/ibis/backends/bigquery/compiler.py
+++ third_party/bigframes_vendored/ibis/backends/bigquery/compiler.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 import ibis.backends.bigquery.compiler as bq_compiler
 import ibis.backends.sql.compiler as sql_compiler
+import ibis.backends.sql.datatypes as sql_datatypes
 import ibis.expr.operations.reductions as ibis_reductions
 import sqlglot as sg
 import sqlglot.expressions as sge
@@ -19,21 +20,43 @@ class BigQueryCompiler(bq_compiler.BigQueryCompiler):
 
     def visit_InMemoryTable(self, op, *, name, schema, data):
         # Avoid creating temp tables for small data, which is how memtable is
-        # used in BigQuery DataFrames. Implementation from:
+        # used in BigQuery DataFrames. Inspired by:
         # https://github.com/ibis-project/ibis/blob/efa6fb72bf4c790450d00a926d7bd809dade5902/ibis/backends/druid/compiler.py#L95
         tuples = data.to_frame().itertuples(index=False)
         quoted = self.quoted
         columns = [sg.column(col, quoted=quoted) for col in schema.names]
-        expr = sge.Values(
+        expr = sge.Unnest(
             expressions=[
-                sge.Tuple(expressions=tuple(map(sge.convert, row))) for row in tuples
+                sge.DataType(
+                    this=sge.DataType.Type.ARRAY,
+                    expressions=[
+                        # TODO: Data types and names from schema.
+                        sge.DataType(
+                            this=sge.DataType.Type.STRUCT,
+                            expressions=[
+                                sge.ColumnDef(
+                                    this=sge.to_identifier(field, quoted=self.quoted),
+                                    kind=sql_datatypes.SqlglotType.from_ibis(type_),
+                                )
+                                for field, type_ in zip(schema.names, schema.types)
+                            ],
+                            nested=True,
+                        )
+                    ],
+                    nested=True,
+                    values=[
+                        sge.Tuple(expressions=tuple(map(sge.convert, row)))
+                        for row in tuples
+                    ],
+                ),
             ],
             alias=sge.TableAlias(
                 this=sg.to_identifier(name, quoted=quoted),
                 columns=columns,
             ),
         )
-        return sg.select(*columns).from_(expr)
+        # return expr
+        return sg.select(sge.Star()).from_(expr)
 
     def visit_FirstNonNullValue(self, op, *, arg):
         return sge.IgnoreNulls(this=sge.FirstValue(this=arg))
