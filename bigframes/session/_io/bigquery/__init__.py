840523b203d03d2be293fcddec9366f151e77e19
Huan Chen <huanc@google.com>
2024-08-14 21:47:00 +0000

    chore: update benchmark logic

--- bigframes/session/_io/bigquery/__init__.py
+++ bigframes/session/_io/bigquery/__init__.py
@@ -252,17 +252,19 @@ def start_query_with_client(
 
     stats = get_performance_stats(query_job)
     if stats is not None:
-        bytes_processed, slot_millis = stats
+        bytes_processed, slot_millis, exec_seconds = stats
         session._add_bytes_processed(bytes_processed)
         session._add_slot_millis(slot_millis)
         if LOGGING_NAME_ENV_VAR in os.environ:
             # when running notebooks via pytest nbmake
-            write_stats_to_disk(bytes_processed, slot_millis)
+            write_stats_to_disk(bytes_processed, slot_millis, exec_seconds)
 
     return results_iterator, query_job
 
 
-def get_performance_stats(query_job: bigquery.QueryJob) -> Optional[Tuple[int, int]]:
+def get_performance_stats(
+    query_job: bigquery.QueryJob,
+) -> Optional[Tuple[int, int, float]]:
     """Parse the query job for performance stats.
 
     Return None if the stats do not reflect real work done in bigquery.
@@ -277,14 +279,21 @@ def get_performance_stats(query_job: bigquery.QueryJob) -> Optional[Tuple[int, i
     slot_millis = query_job.slot_millis
     if not isinstance(slot_millis, int):
         return None  # filter out mocks
+
     if query_job.configuration.dry_run:
         # dry run stats are just predictions of the real run
         slot_millis = 0
 
-    return bytes_processed, slot_millis
+    exec_seconds = (
+        (query_job.ended - query_job.created).total_seconds()
+        if query_job.created is not None and query_job.ended is not None
+        else None
+    )
+
+    return bytes_processed, slot_millis, exec_seconds
 
 
-def write_stats_to_disk(bytes_processed: int, slot_millis: int):
+def write_stats_to_disk(bytes_processed: int, slot_millis: int, exec_seconds: float):
     """For pytest runs only, log information about the query job
     to a file in order to create a performance report.
     """
@@ -303,9 +312,16 @@ def write_stats_to_disk(bytes_processed: int, slot_millis: int):
         f.write(str(bytes_processed) + "\n")
 
     # store slot milliseconds
-    bytes_file = os.path.join(current_directory, test_name + ".slotmillis")
-    with open(bytes_file, "a") as f:
+    slot_file = os.path.join(current_directory, test_name + ".slotmillis")
+    with open(slot_file, "a") as f:
         f.write(str(slot_millis) + "\n")
+
+    # store execution time seconds
+    exec_time_file = os.path.join(
+        current_directory, test_name + ".bq_exec_time_seconds"
+    )
+    with open(exec_time_file, "a") as f:
+        f.write(str(exec_seconds) + "\n")
 
 
 def delete_tables_matching_session_id(
