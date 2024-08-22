f92e1ae43b8cc63bec7b76930ffc3600b889806e
Trevor Bergeron <tbergeron@google.com>
2024-08-22 00:44:33 +0000

    refactor: Extract data loading logic into class

--- bigframes/session/_io/bigquery/__init__.py
+++ bigframes/session/_io/bigquery/__init__.py
@@ -82,20 +82,6 @@ def create_job_configs_labels(
     return dict(zip(labels[:MAX_LABELS_COUNT], values[:MAX_LABELS_COUNT]))
 
 
-def create_export_csv_statement(
-    table_id: str, uri: str, field_delimiter: str, header: bool
-) -> str:
-    return create_export_data_statement(
-        table_id,
-        uri,
-        "CSV",
-        {
-            "field_delimiter": field_delimiter,
-            "header": header,
-        },
-    )
-
-
 def create_export_data_statement(
     table_id: str, uri: str, format: str, export_options: Dict[str, Union[bool, str]]
 ) -> str:
