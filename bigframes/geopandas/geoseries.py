76eaf4d9ae101af2562ec9033569f530cafb8f47
Arwa <arwas@google.com>
2024-09-17 15:40:02 -0500

    Added a constructor

--- bigframes/geopandas/geoseries.py
+++ bigframes/geopandas/geoseries.py
@@ -11,11 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations
 
 import bigframes_vendored.geopandas.geoseries as vendored_geoseries
+import geopandas.array
 
 import bigframes.series
 
 
 class GeoSeries(vendored_geoseries.GeoSeries, bigframes.series.Series):
-    pass
+    def __init__(self, data=None, index=None, **kwargs):
+        super().__init__(
+            data=data, index=index, dtype=geopandas.array.GeometryDtype(), **kwargs
+        )
