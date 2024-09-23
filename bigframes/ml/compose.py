cc9840c52c8f44476718529b55ab54aa24c75861
Ferenc Hechler <github@fh.anderemails.de>
2024-09-23 22:11:38 +0200

    remove filter for "ML." sqls in _extract_output_names() of BaseTransformer

--- bigframes/ml/compose.py
+++ bigframes/ml/compose.py
@@ -295,22 +295,6 @@ class ColumnTransformer(
         self._extract_output_names()
         return self
 
-    # Overwrite the implementation in BaseTransformer, as it only supports the "ML." transformers.
-    # TODO: clarify if this should be changed in base.BaseTransformer?
-    def _extract_output_names(self):
-        """Extract transform output column names. Save the results to self._output_names."""
-        assert self._bqml_model is not None
-
-        output_names = []
-        for transform_col in self._bqml_model._model._properties["transformColumns"]:
-            transform_col_dict = cast(dict, transform_col)
-            # pass the columns that are not transformed
-            if "transformSql" not in transform_col_dict:
-                continue
-            output_names.append(transform_col_dict["name"])
-
-        self._output_names = output_names
-
     def transform(self, X: Union[bpd.DataFrame, bpd.Series]) -> bpd.DataFrame:
         if not self._bqml_model:
             raise RuntimeError("Must be fitted before transform")
