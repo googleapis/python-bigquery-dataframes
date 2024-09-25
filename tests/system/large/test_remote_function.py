8fae9763c52a8d00b45c1ee6cae1eeb4fffb9b21
Shobhit Singh <shobs@google.com>
2024-09-25 07:59:58 +0000

    nit reword comment

--- tests/system/large/test_remote_function.py
+++ tests/system/large/test_remote_function.py
@@ -1751,7 +1751,7 @@ def test_df_apply_axis_1_complex(session, pd_df):
         bf_result = bf_df.apply(serialize_row_remote, axis=1).to_pandas()
         pd_result = pd_df.apply(serialize_row, axis=1)
 
-        # Ignore known dtype disparities between pandas and bigframes.
+        # ignore known dtype difference between pandas and bigframes
         pandas.testing.assert_series_equal(
             pd_result, bf_result, check_dtype=False, check_index_type=False
         )
