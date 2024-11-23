a0168518b92381a558a721747efaa8350aca727c
Chelsea Lin <chelsealin@google.com>
2024-11-23 02:10:44 +0000

    chore: vendor ibis.expr.operations.reductions and ibis.expr.operations.core

--- third_party/bigframes_vendored/ibis/expr/operations/json.py
+++ third_party/bigframes_vendored/ibis/expr/operations/json.py
@@ -1,8 +1,8 @@
 # Contains code from https://github.com/ibis-project/ibis/blob/master/ibis/expr/operations/json.py
 from __future__ import annotations
 
+import bigframes_vendored.ibis.expr.operations.core as ibis_ops_core
 import ibis.expr.datatypes as dt
-import ibis.expr.operations.core as ibis_ops_core
 
 
 # TODO(swast): Remove once supported upstream.
--- third_party/bigframes_vendored/ibis/expr/operations/reductions.py
+++ third_party/bigframes_vendored/ibis/expr/operations/reductions.py
@@ -1,12 +1,29 @@
-# Contains code from https://github.com/ibis-project/ibis/blob/master/ibis/expr/operations/reductions.py
+# Contains code from https://github.com/ibis-project/ibis/blob/9.2.0/ibis/expr/operations/reductions.py
+
+"""Reduction operations."""
 
 from __future__ import annotations
 
+from typing import Optional
+
 import bigframes_vendored.ibis.common.annotations as ibis_annotations
 from bigframes_vendored.ibis.common.typing import VarTuple
+from bigframes_vendored.ibis.expr.operations.core import Column, Value
+import ibis.expr.datashape as ds
 import ibis.expr.datatypes as dt
-import ibis.expr.operations.core as ibis_ops_core
-from ibis.expr.operations.reductions import Filterable, Reduction
+from public import public
+
+
+@public
+class Reduction(Value):
+    """Base class for reduction operations."""
+
+    shape = ds.scalar
+
+
+# TODO(kszucs): all reductions all filterable so we could remove Filterable
+class Filterable(Value):
+    where: Optional[Value[dt.Boolean]] = None
 
 
 class ArrayAggregate(Filterable, Reduction):
@@ -15,8 +32,8 @@ class ArrayAggregate(Filterable, Reduction):
     the ibis `ArrayCollect`, but adds `order_by_*` and `distinct_only` parameters.
     """
 
-    arg: ibis_ops_core.Column
-    order_by: VarTuple[ibis_ops_core.Value] = ()
+    arg: Column
+    order_by: VarTuple[Value] = ()
 
     @ibis_annotations.attribute
     def dtype(self):
