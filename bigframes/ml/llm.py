6a44e7b6c7cbd7542be12865019dafab105c1f1e
Shuowei Li <shuowei@google.com>
2024-12-13 19:28:02 +0000

    Revert "merge genimi_fine_tune_endpoints and genimi_score_endpoints, since they are identical"
    This reverts commit 205e1738d6d21edfd9ad8f753fd659c248b54475.

--- bigframes/ml/llm.py
+++ bigframes/ml/llm.py
@@ -895,7 +895,8 @@ class GeminiTextGenerator(base.BaseEstimator):
         X: utils.ArrayType,
         y: utils.ArrayType,
     ) -> GeminiTextGenerator:
-        """Fine tune GeminiTextGenerator model. Only support "gemini-pro" and "gemini-1.5" model for now.
+        """Fine tune GeminiTextGenerator model. Only support "gemini-pro", "gemini-1.5-pro-002",
+           "gemini-1.5-flash-002" models for now.
 
         .. note::
 
@@ -913,11 +914,10 @@ class GeminiTextGenerator(base.BaseEstimator):
         Returns:
             GeminiTextGenerator: Fitted estimator.
         """
-        # Support gemini-1.5 and gemini-pro
-        supported_models = ["gemini-pro", "gemini-1.5-pro-002", "gemini-1.5-flash-002"]
-        if self.model_name not in supported_models:
+        if self.model_name not in _GEMINI_FINE_TUNE_SCORE_ENDPOINTS:
             raise NotImplementedError(
-                "Score is not supported models other than gemini-pro or gemini-1.5 model."
+                "fit() only supports gemini-pro, \
+                    gemini-1.5-pro-002, or gemini-1.5-flash-002 model."
             )
 
         X, y = utils.batch_convert_to_dataframe(X, y)
@@ -1036,7 +1036,7 @@ class GeminiTextGenerator(base.BaseEstimator):
             "text_generation", "classification", "summarization", "question_answering"
         ] = "text_generation",
     ) -> bpd.DataFrame:
-        """Calculate evaluation metrics of the model. Only "gemini-pro" and "gemini-1.5" model is supported for now.
+        """Calculate evaluation metrics of the model. Only support "gemini-pro" and "gemini-1.5-pro-002", and "gemini-1.5-flash-002".
 
         .. note::
 
@@ -1068,11 +1068,10 @@ class GeminiTextGenerator(base.BaseEstimator):
         if not self._bqml_model:
             raise RuntimeError("A model must be fitted before score")
 
-        # Support gemini-1.5 and gemini-pro
-        supported_models = ["gemini-pro", "gemini-1.5-pro-002", "gemini-1.5-flash-002"]
-        if self.model_name not in supported_models:
+        if self.model_name not in _GEMINI_FINE_TUNE_SCORE_ENDPOINTS:
             raise NotImplementedError(
-                "Score is not supported models other than gemini-pro or gemini-1.5 model."
+                "score() only supports gemini-pro \
+                , gemini-1.5-pro-002, and gemini-1.5-flash-2 model."
             )
 
         X, y = utils.batch_convert_to_dataframe(X, y, session=self._bqml_model.session)
