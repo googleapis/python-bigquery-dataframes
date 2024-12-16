b44bcae283e4f2e6206eb4ecb804c8e9ddc4e4a4
rey-esp <drespana@google.com>
2024-12-16 11:35:36 -0600

    Update test_geoseries.py

--- tests/system/small/geopandas/test_geoseries.py
+++ tests/system/small/geopandas/test_geoseries.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import geopandas
+import geopandas  # type: ignore
 import google.api_core.exceptions
 import pandas as pd
 import pytest
