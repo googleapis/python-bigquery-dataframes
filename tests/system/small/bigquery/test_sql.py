6c86671f35b7aaea27450220e7ad6974b6f09632
Tim Swena <swast@google.com>
2025-01-16 12:29:43 -0600

    fix ibis compilation and add a test

---
+++ tests/system/small/bigquery/test_sql.py
@@ -0,0 +1,30 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import bigframes.bigquery
+import bigframes.pandas as bpd
+
+
+def test_sql_scalar():
+    df = bpd.read_gbq("bigquery-public-data.ml_datasets.penguins")
+
+    shortest = bigframes.bigquery.sql_scalar(
+        "LEAST({0}, {1}, {2})",
+        columns=[
+            df["culmen_depth_mm"],
+            df["culmen_length_mm"],
+            df["flipper_length_mm"],
+        ],
+    )
+    shortest.peek()
