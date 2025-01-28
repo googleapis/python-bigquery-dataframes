0acc9cd9512f5ea089bc30564d4f6b2856b617ad
Shuowei Li <shuowei@google.com>
2025-01-28 00:04:10 +0000

    change the testcase expected results to improve readability

--- tests/system/small/ml/test_core.py
+++ tests/system/small/ml/test_core.py
@@ -430,28 +430,49 @@ def test_model_forecast(
             {"horizon": 4, "confidence_level": 0.8}
         )
     ).to_pandas()[forecast_cols]
-    expected = pd.DataFrame(
-        {
-            "forecast_timestamp": [
-                datetime(2017, 8, 2, tzinfo=utc),
-                datetime(2017, 8, 3, tzinfo=utc),
-                datetime(2017, 8, 4, tzinfo=utc),
-                datetime(2017, 8, 5, tzinfo=utc),
-            ],
-            "forecast_value": [2634.796023, 2621.332462, 2396.095463, 1742.878278],
-        }
-    )
+    if id_col_name:
+        expected = pd.DataFrame(
+            {
+                "id": ["1", "2", "1", "2", "1", "2", "1", "2"],
+                "forecast_timestamp": [
+                    datetime(2017, 8, 2, tzinfo=utc),
+                    datetime(2017, 8, 2, tzinfo=utc),
+                    datetime(2017, 8, 3, tzinfo=utc),
+                    datetime(2017, 8, 3, tzinfo=utc),
+                    datetime(2017, 8, 4, tzinfo=utc),
+                    datetime(2017, 8, 4, tzinfo=utc),
+                    datetime(2017, 8, 5, tzinfo=utc),
+                    datetime(2017, 8, 5, tzinfo=utc),
+                ],
+                "forecast_value": [
+                    2634.796023,
+                    2634.796023,
+                    2621.332462,
+                    2621.332462,
+                    2396.095463,
+                    2396.095463,
+                    1742.878278,
+                    1742.878278,
+                ],
+            }
+        )
+        expected["id"] = expected["id"].astype("string[pyarrow]")
+    else:
+        expected = pd.DataFrame(
+            {
+                "forecast_timestamp": [
+                    datetime(2017, 8, 2, tzinfo=utc),
+                    datetime(2017, 8, 3, tzinfo=utc),
+                    datetime(2017, 8, 4, tzinfo=utc),
+                    datetime(2017, 8, 5, tzinfo=utc),
+                ],
+                "forecast_value": [2634.796023, 2621.332462, 2396.095463, 1742.878278],
+            }
+        )
     expected["forecast_value"] = expected["forecast_value"].astype(pd.Float64Dtype())
     expected["forecast_timestamp"] = expected["forecast_timestamp"].astype(
         pd.ArrowDtype(pa.timestamp("us", tz="UTC"))
     )
-    if id_col_name:
-        expected_expanded = expected.loc[expected.index.repeat(2)].reset_index(
-            drop=True
-        )
-        expected_expanded.insert(0, "id", ["1", "2", "1", "2", "1", "2", "1", "2"])
-        expected_expanded["id"] = expected_expanded["id"].astype("string[pyarrow]")
-        expected = expected_expanded
     pd.testing.assert_frame_equal(
         forecast,
         expected,
