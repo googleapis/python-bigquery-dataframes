a2edcbf650e9874116d8e642fa658d012c38226c
Chelsea Lin <chelsealin@google.com>
2025-03-06 23:55:13 +0000

    fix test_df_drop_duplicates_w_json

--- bigframes/core/array_value.py
+++ bigframes/core/array_value.py
@@ -108,7 +108,7 @@ class ArrayValue:
             raise ValueError("must set at most one of 'offests', 'primary_key'")
         if any(i.field_type == "JSON" for i in table.schema if i.name in schema.names):
             msg = bfe.format_message(
-                "JSON column interpretation as a custom PyArrow extention in `db_dtypes`  "
+                "JSON column interpretation as a custom PyArrow extention in `db_dtypes` "
                 "is a preview feature and subject to change."
             )
             warnings.warn(msg, bfe.PreviewWarning)
