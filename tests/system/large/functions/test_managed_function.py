2fd85ee59e507704531dda2bfeafaa2840a0912e
Shobhit Singh <shobs@google.com>
2025-03-08 01:27:29 +0000

    remove pytest.mark.skip from one more test

--- tests/system/large/functions/test_managed_function.py
+++ tests/system/large/functions/test_managed_function.py
@@ -13,10 +13,7 @@
 # limitations under the License.
 
 import pandas
-import pytest
 
-from bigframes.functions import _function_session as bff_session
-from bigframes.functions._utils import get_python_version
 import bigframes.pandas as bpd
 from tests.system.utils import cleanup_function_assets
 
@@ -122,10 +119,6 @@ def test_managed_function_stringify_with_ibis(
         )
 
 
-@pytest.mark.skipif(
-    get_python_version() not in bff_session._MANAGED_FUNC_PYTHON_VERSIONS,
-    reason=f"Supported version: {bff_session._MANAGED_FUNC_PYTHON_VERSIONS}",
-)
 def test_managed_function_binop(session, scalars_dfs, dataset_id):
     try:
 
