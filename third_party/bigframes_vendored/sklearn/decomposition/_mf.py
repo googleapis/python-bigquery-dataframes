63e8e9c3b0fbf4a4c62130355eb021aff34868a4
Daniela <drespana@google.com>
2025-03-18 22:56:41 +0000

    fit

--- third_party/bigframes_vendored/sklearn/decomposition/_mf.py
+++ third_party/bigframes_vendored/sklearn/decomposition/_mf.py
@@ -26,8 +26,7 @@ class MatrixFactorization(BaseEstimator, metaclass=ABCMeta):
         >>> from bigframes.ml.decomposition import MatrixFactorization
         >>> X = bpd.DataFrame([[1, 1], [2, 1], [3, 1.2], [4, 1], [5, 0.8], [6, 1]])
         >>> model = MatrixFactorization(feedback_type='explicit', num_factors=6, user_col='user_id', item_col='item_col', l2_reg=2.06)
-        >>> W = model.fit(X)
-        >>> result = model.global_explain()
+        >>> W = model.fit(model.fit(X.rename(columns={'rating': 'rating_col', 'item_id': 'item_col'})))
 
     Args:
         feedback_type ('explicit' | 'implicit'):
