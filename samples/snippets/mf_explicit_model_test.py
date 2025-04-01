8703eb4fe184b1987a3513fe87dd3316cc052117
Daniela <drespana@google.com>
2025-04-01 22:17:59 +0000

    docs: add matrix_factorization snippets

---
+++ samples/snippets/mf_explicit_model_test.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (t
+# you may not use this file except in compliance wi
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in
+# distributed under the License is distributed on a
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, eit
+# See the License for the specific language governi
+# limitations under the License.
+
+
+def test_explicit_matrix_factorization(random_model_id: str) -> None:
+    pass
