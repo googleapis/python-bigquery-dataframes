14841efb4d5e85fb73dbb6a9d517fa437e4e5f68
Tim Swena <swast@google.com>
2025-04-21 17:03:52 -0500

    add tests

--- tests/unit/core/test_dtypes.py
+++ tests/unit/core/test_dtypes.py
@@ -20,7 +20,7 @@ import numpy as np
 import pandas as pd
 import pyarrow as pa  # type: ignore
 import pytest
-import shapely
+import shapely  # type: ignore
 
 import bigframes.core.compile.ibis_types
 import bigframes.dtypes
