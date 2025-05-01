33f446a1cf70ed6440538b20776bf5f972bb5437
Daniela <drespana@google.com>
2025-05-01 19:06:36 +0000

    implicit create

--- samples/snippets/mf_implicit_model_test.py
+++ samples/snippets/mf_implicit_model_test.py
@@ -25,20 +25,23 @@ def test_implicit_matrix_factorization(random_model_id: str) -> None:
     #
     # [END bigframes_dataframe_mf_implicit_data]
     # [START bigframes_dataframe_mf_implicit_model]
-    rating = 0.3 * (1 + (bq_df["session_duration"] - 57937) / 57937)
+    rating_calculation = 0.3 * (1 + (bq_df["session_duration"] - 57937) / 57937)
+    filtered_bq_df = bq_df[rating_calculation < 1].assign(
+        rating=rating_calculation[rating_calculation < 1]
+    )
     model = decomposition.MatrixFactorization(
         num_factors=15,
         feedback_type="implicit",
         user_col="visitorId",
         item_col="contentId",
-        rating_col=rating,
+        rating_col="rating",
         l2_reg=30,
     )
-    # condition of rating < 1 required before fitting model
-
+    model.fit(filtered_bq_df)
     # [END bigframes_dataframe_mf_implicit_model]
     # [START bigframes_dataframe_mf_implicit_evaluate]
-    model.fit(bq_df)
+    model.score()
+    # Output:
     # [END bigframes_dataframe_mf_implicit_evaluate]
     # [START bigframes_dataframe_mf_implicit_subset]
     # [END bigframes_dataframe_mf_implicit_subset]
