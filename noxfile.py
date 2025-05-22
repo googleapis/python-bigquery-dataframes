440eacfa185dbd4d1f7a5c15c0aa408624606811
Chelsea Lin <chelsealin@google.com>
2025-05-22 21:27:34 +0000

    chore: add individual timeout for unit-tests

--- noxfile.py
+++ noxfile.py
@@ -70,9 +70,10 @@ UNIT_TEST_STANDARD_DEPENDENCIES = [
     "mock",
     "asyncmock",
     PYTEST_VERSION,
-    "pytest-cov",
     "pytest-asyncio",
+    "pytest-cov",
     "pytest-mock",
+    "pytest-timeout",
 ]
 UNIT_TEST_LOCAL_DEPENDENCIES: List[str] = []
 UNIT_TEST_DEPENDENCIES: List[str] = []
@@ -228,6 +229,10 @@ def run_unit(session, install_test_extra):
     session.run(
         "py.test",
         "--quiet",
+        # Any individual test taking longer than 1 mins will be terminated.
+        "--timeout=60",
+        # Log 20 slowest tests
+        "--durations=20",
         f"--junitxml=unit_{session.python}_sponge_log.xml",
         "--cov=bigframes",
         f"--cov={tests_path}",
