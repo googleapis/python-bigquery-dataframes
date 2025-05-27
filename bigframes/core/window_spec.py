ecf6e804a34dcc36049bfd7a7f1705562a7970aa
Trevor Bergeron <tbergeron@google.com>
2025-05-27 20:45:53 +0000

    fix row bounded def

--- bigframes/core/window_spec.py
+++ bigframes/core/window_spec.py
@@ -234,7 +234,9 @@ class WindowSpec:
         This is relevant for determining whether the window requires a total order
         to calculate deterministically.
         """
-        return isinstance(self.bounds, RowsWindowBounds)
+        return isinstance(self.bounds, RowsWindowBounds) and (
+            (self.bounds.start is not None) or (self.bounds.end is not None)
+        )
 
     @property
     def is_range_bounded(self):
