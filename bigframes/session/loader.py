645316d7e5f19712fa27e5c1a05c0ecd5777c856
Chelsea Lin <chelsealin@google.com>
2025-06-03 22:52:33 +0000

    ensure columns is not list type and avoid flacky ordered of columns

--- bigframes/session/loader.py
+++ bigframes/session/loader.py
@@ -98,7 +98,7 @@ def _to_index_cols(
 
 def _check_column_duplicates(
     index_cols: Iterable[str], columns: Iterable[str], is_index_in_columns: bool
-) -> List[str]:
+) -> Iterable[str]:
     index_cols_list = list(index_cols) if index_cols is not None else []
     columns_list = list(columns) if columns is not None else []
     set_index = set(index_cols_list)
@@ -111,7 +111,7 @@ def _check_column_duplicates(
         )
 
     if len(columns_list) == 0:
-        return columns_list
+        return columns
 
     if len(columns_list) > len(set_columns):
         raise ValueError(
@@ -125,14 +125,14 @@ def _check_column_duplicates(
                 f"The specified index column(s) were not found: {set_index - set_columns}. "
                 f"Available columns are: {set_columns}"
             )
-        return list(set_columns - set_index)
+        return [col for col in columns if col not in set_index]
     else:
         if not set_index.isdisjoint(set_columns):
             raise ValueError(
                 "Found column names that exist in both 'index_col' and 'columns' arguments. "
                 "These arguments must specify distinct sets of columns."
             )
-        return columns_list
+        return columns
 
 
 @dataclasses.dataclass
@@ -533,7 +533,9 @@ class GbqDataLoader:
             index_col=index_col,
             names=names,
         )
-        columns = _check_column_duplicates(index_cols, columns, is_index_in_columns)
+        columns = list(
+            _check_column_duplicates(index_cols, columns, is_index_in_columns)
+        )
 
         for key in index_cols:
             if key not in table_column_names:
