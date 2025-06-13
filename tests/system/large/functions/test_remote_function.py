517686c9f984a686e58998d64e165942ac18b51d
Trevor Bergeron <tbergeron@google.com>
2025-06-13 17:03:32 +0000

    remove unwanted test

--- tests/system/large/functions/test_remote_function.py
+++ tests/system/large/functions/test_remote_function.py
@@ -97,63 +97,6 @@ def bq_cf_connection() -> str:
 
 
 @pytest.mark.flaky(retries=2, delay=120)
-def test_remote_function_multiply_with_ibis(
-    session,
-    scalars_table_id,
-    bigquery_client,
-    ibis_client,
-    dataset_id,
-    bq_cf_connection,
-):
-    try:
-
-        @session.remote_function(
-            # Make sure that the input/output types can be used positionally.
-            # This avoids the worst of the breaking change from 1.x to 2.x.
-            [int, int],
-            int,
-            dataset_id,
-            bigquery_connection=bq_cf_connection,
-            reuse=False,
-            cloud_function_service_account="default",
-        )
-        def multiply(x, y):
-            return x * y
-
-        _, dataset_name, table_name = scalars_table_id.split(".")
-        if not ibis_client.dataset:
-            ibis_client.dataset = dataset_name
-
-        col_name = "int64_col"
-        table = ibis_client.tables[table_name]
-        table = table.filter(table[col_name].notnull()).order_by("rowindex").head(10)
-        sql = table.compile()
-        pandas_df_orig = bigquery_client.query(sql).to_dataframe()
-
-        col = table[col_name]
-        col_2x = multiply(col, 2).name("int64_col_2x")
-        col_square = multiply(col, col).name("int64_col_square")
-        table = table.mutate([col_2x, col_square])
-        sql = table.compile()
-        pandas_df_new = bigquery_client.query(sql).to_dataframe()
-
-        pandas.testing.assert_series_equal(
-            pandas_df_orig[col_name] * 2,
-            pandas_df_new["int64_col_2x"],
-            check_names=False,
-        )
-
-        pandas.testing.assert_series_equal(
-            pandas_df_orig[col_name] * pandas_df_orig[col_name],
-            pandas_df_new["int64_col_square"],
-            check_names=False,
-        )
-    finally:
-        # clean up the gcp assets created for the remote function
-        cleanup_function_assets(multiply, bigquery_client, session.cloudfunctionsclient)
-
-
-@pytest.mark.flaky(retries=2, delay=120)
 def test_remote_function_binop(session, scalars_dfs, dataset_id, bq_cf_connection):
     try:
 
