5f6184319160aa0fb3a5ba0e2626f6b4b745d444
Shobhit Singh <shobs@google.com>
2025-06-26 00:26:31 +0000

    import module rephrasing

--- bigframes/clients.py
+++ bigframes/clients.py
@@ -24,7 +24,7 @@ from typing import cast, Optional
 import google.api_core.exceptions
 import google.api_core.retry
 from google.cloud import bigquery_connection_v1, resourcemanager_v3
-import google.iam.v1
+from google.iam.v1 import policy_pb2
 
 logger = logging.getLogger(__name__)
 
@@ -173,9 +173,7 @@ class BqConnectionManager:
                     return
 
         # Create a new binding
-        new_binding = google.iam.v1.policy_pb2.Binding(
-            role=role, members=[service_account]
-        )  # Use a dictionary to avoid problematic google.iam namespace package.
+        new_binding = policy_pb2.Binding(role=role, members=[service_account])
         policy.bindings.append(new_binding)
         request = {
             "resource": project,
