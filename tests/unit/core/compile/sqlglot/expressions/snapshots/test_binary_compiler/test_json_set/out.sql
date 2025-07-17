3cb3d79e796dc6a863812ac7db96b14d6634b0f9
Chelsea Lin <chelsealin@google.com>
2025-07-17 23:45:01 +0000

    chore: compare SQLs generated by Ibis and sqlglot

--- tests/unit/core/compile/sqlglot/expressions/snapshots/test_binary_compiler/test_json_set/out.sql
+++ tests/unit/core/compile/sqlglot/expressions/snapshots/test_binary_compiler/test_json_set/out.sql
@@ -1,20 +1,8 @@
-WITH `bfcte_0` AS (
-  SELECT
-    `rowindex` AS `bfcol_0`,
-    `json_col` AS `bfcol_1`
-  FROM `bigframes-dev`.`sqlglot_test`.`json_types`
-), `bfcte_1` AS (
-  SELECT
-    *,
-    JSON_SET(`bfcol_1`, '$.a', 100) AS `bfcol_4`
-  FROM `bfcte_0`
-), `bfcte_2` AS (
-  SELECT
-    *,
-    JSON_SET(`bfcol_4`, '$.b', 'hi') AS `bfcol_7`
-  FROM `bfcte_1`
-)
 SELECT
-  `bfcol_0` AS `rowindex`,
-  `bfcol_7` AS `json_col`
-FROM `bfcte_2`
+  `t1`.`rowindex`,
+  json_set(json_set(`t1`.`json_col`, '$.a', 100), '$.b', 'hi') AS `json_col`
+FROM (
+  SELECT
+    *
+  FROM `bigframes-dev.sqlglot_test.json_types` AS `t0`
+) AS `t1`
--- tests/unit/core/compile/sqlglot/snapshots/test_compile_readtable/test_compile_readtable_w_ordering/out.sql
+++ tests/unit/core/compile/sqlglot/snapshots/test_compile_readtable/test_compile_readtable_w_ordering/out.sql
@@ -1,12 +1,9 @@
-WITH `bfcte_0` AS (
-  SELECT
-    `int64_col` AS `bfcol_0`,
-    `rowindex` AS `bfcol_1`
-  FROM `bigframes-dev`.`sqlglot_test`.`scalar_types`
-)
 SELECT
-  `bfcol_1` AS `rowindex`,
-  `bfcol_0` AS `int64_col`
-FROM `bfcte_0`
-ORDER BY
-  `bfcol_0` ASC NULLS LAST
+`rowindex` AS `rowindex`,
+`int64_col` AS `int64_col`
+FROM
+(SELECT
+  `t0`.`rowindex`,
+  `t0`.`int64_col`
+FROM `bigframes-dev.sqlglot_test.scalar_types` AS `t0`)
+ORDER BY `int64_col` ASC NULLS LAST
