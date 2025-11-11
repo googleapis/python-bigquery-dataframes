43e44a78269f393ec3756bb67af3d175aa48ce49
jialuo <jialuo@google.com>
2025-11-11 01:00:16 +0000

    update sql

--- tests/unit/core/compile/sqlglot/expressions/snapshots/test_numeric_ops/test_euclidean_distance/out.sql
+++ tests/unit/core/compile/sqlglot/expressions/snapshots/test_numeric_ops/test_euclidean_distance/out.sql
@@ -1,13 +1,13 @@
 WITH `bfcte_0` AS (
   SELECT
-    `int_list_col` AS `bfcol_0`,
-    `numeric_list_col` AS `bfcol_1`
+    `int_list_col`,
+    `numeric_list_col`
   FROM `bigframes-dev`.`sqlglot_test`.`repeated_types`
 ), `bfcte_1` AS (
   SELECT
     *,
-    ML.DISTANCE(`bfcol_0`, `bfcol_0`, 'EUCLIDEAN') AS `bfcol_2`,
-    ML.DISTANCE(`bfcol_1`, `bfcol_1`, 'EUCLIDEAN') AS `bfcol_3`
+    ML.DISTANCE(`int_list_col`, `int_list_col`, 'EUCLIDEAN') AS `bfcol_2`,
+    ML.DISTANCE(`numeric_list_col`, `numeric_list_col`, 'EUCLIDEAN') AS `bfcol_3`
   FROM `bfcte_0`
 )
 SELECT
