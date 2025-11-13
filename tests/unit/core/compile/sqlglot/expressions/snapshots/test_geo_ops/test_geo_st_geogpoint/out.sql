8e1fe470958be6d0906d2a7bf868aaca038065e4
jialuo <jialuo@google.com>
2025-11-13 22:46:14 +0000

    fix sql

--- tests/unit/core/compile/sqlglot/expressions/snapshots/test_geo_ops/test_geo_st_geogpoint/out.sql
+++ tests/unit/core/compile/sqlglot/expressions/snapshots/test_geo_ops/test_geo_st_geogpoint/out.sql
@@ -1,12 +1,12 @@
 WITH `bfcte_0` AS (
   SELECT
-    `rowindex` AS `bfcol_0`,
-    `rowindex_2` AS `bfcol_1`
+    `rowindex`,
+    `rowindex_2`
   FROM `bigframes-dev`.`sqlglot_test`.`scalar_types`
 ), `bfcte_1` AS (
   SELECT
     *,
-    ST_GEOGPOINT(`bfcol_0`, `bfcol_1`) AS `bfcol_2`
+    ST_GEOGPOINT(`rowindex`, `rowindex_2`) AS `bfcol_2`
   FROM `bfcte_0`
 )
 SELECT
