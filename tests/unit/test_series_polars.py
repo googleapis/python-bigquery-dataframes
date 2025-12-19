9dff0f42fb8f58626c26a705ed6fb68e612223c3
Shuowei Li <shuowei@google.com>
2025-12-19 20:18:20 +0000

    Revert "test: update old testcase due to new feature implementation"
    This reverts commit c39293a6ec80fdb5331d720bd06dec9368bb64d4.

--- tests/unit/test_series_polars.py
+++ tests/unit/test_series_polars.py
@@ -2020,8 +2020,9 @@ def test_series_small_repr(scalars_dfs):
     bf_series = scalars_df[col_name]
     pd_series = scalars_pandas_df[col_name]
     with bigframes.pandas.option_context("display.repr_mode", "head"):
-        pd_string = pd_series.to_string(length=True, dtype=True, name=True)
-        assert repr(bf_series) == f"{pd_string}\n\n[{len(pd_series)} rows]"
+        assert repr(bf_series) == pd_series.to_string(
+            length=False, dtype=True, name=True
+        )
 
 
 def test_sum(scalars_dfs):
