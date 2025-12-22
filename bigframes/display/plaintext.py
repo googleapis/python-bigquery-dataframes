d36fc0fe15986a10b579ddda089ecf8ebb129fbc
Shuowei Li <shuowei@google.com>
2025-12-22 22:27:07 +0000

    refactor: move code to plaintext file and add checks

---
+++ bigframes/display/plaintext.py
@@ -0,0 +1,35 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Plaintext display representations."""
+
+from __future__ import annotations
+
+import typing
+
+if typing.TYPE_CHECKING:
+    import pandas as pd
+
+    import bigframes
+
+
+def create_text_representation(
+    obj: "bigframes.dataframe.DataFrame" | "bigframes.series.Series",
+    pandas_df: "pd.DataFrame",
+    total_rows: int | None,
+) -> str:
+    """Create a text representation of the DataFrame or Series."""
+    # TODO(swast): This module should probably just be removed and combined
+    # with the html module.
+    return obj._create_text_representation(pandas_df, total_rows)
